ccdf468418b379af28373d9d676f432e9a623a03
Joseph York <York.62@Wright.edu>
2026-05-01 03:45:58 -0400

    Added Extended Read capability using a dap struct

--- stage0/src/lib.rs
+++ stage0/src/lib.rs
@@ -42,16 +42,58 @@ pub fn cscrn() {
 pub fn readdsk(addr: *const ()) {
     unsafe {
         asm!(
-        "push si",
-        "mov es, dx",
-        "int 0x13",
-        "pop si",
+            "push si",
+            "mov es, dx",
+            "int 0x13",
+            "pop si",
 
-        in("bx") addr,
-        in("ax") 0x0201 as u16,
-        in("cx") 0x0002 as u16,
-        in("dx") 0x0000 as u16,
+            in("bx") addr,
+            in("ax") 0x0201 as u16,
+            in("cx") 0x0002 as u16,
+            in("dx") 0x0000 as u16,
         )
+    }
+}
+
+pub fn read_sectors(dap: &DiskAddressPacket) {
+    unsafe {
+        asm!(
+            "push ds",
+            "push si",
+            "mov si, cx",
+            "mov ds, bx",
+            "int 0x13",
+            "pop si",
+            "pop ds",
+            in("ah") 0x42 as u8,
+            in("dl") 0x80 as u8,
+            in("cx") dap as *const _ as u16,
+            in("bx") 0,
+            options(nostack)
+        );
+    }
+}
+
+#[repr(C, packed)]
+pub struct DiskAddressPacket {
+    size: u8,
+    reserved: u8,
+    sector_cnt: u16,
+    buffer_off: u16,
+    buffer_seg: u16,
+    lba: u64,
+}
+
+impl DiskAddressPacket {
+    pub fn new(lba: u64, sectors: u16, offset: u16, segment: u16) -> Self {
+        Self {
+            size: 0x10,
+            reserved: 0,
+            sector_cnt: sectors,
+            buffer_off: offset,
+            buffer_seg: segment,
+            lba,
+        }
     }
 }
 
--- stage0/src/main.rs
+++ stage0/src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 use core::arch::asm;
-use jos::{cscrn, readdsk, puts};
+use jos::{DiskAddressPacket, read_sectors, cscrn, puts};
 
 #[unsafe(no_mangle)]
 #[unsafe(link_section = ".startup")]
@@ -24,9 +24,10 @@ pub unsafe extern "C" fn _start() -> ! {
 fn rust_main() -> ! {
     let ptr = 0x1000 as *const ();
     let stage2: fn() -> ! = unsafe { core::mem::transmute(ptr) };
+    let dap = DiskAddressPacket::new(1, 1, 0x1000, 0x0000);
+
     cscrn();
     puts(b"[ Stage0: OK ] Bootsector Loaded at 0x07C00");
-    readdsk(ptr); // Reads disk to 0
-    stage2(); // Jump to the next stage
+    read_sectors(&dap);
+    stage2();
 }
-
